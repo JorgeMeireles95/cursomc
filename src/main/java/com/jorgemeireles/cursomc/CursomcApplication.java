b876215e9a5af2444c8d42089d3c2d8c171216d2
Jorge <jorgeantoniomeireles@gmail.com>
2021-08-04 23:13:49 -0300

    Pedido,EstadoPagamento e Pagamento

--- src/main/java/com/jorgemeireles/cursomc/CursomcApplication.java
+++ src/main/java/com/jorgemeireles/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.jorgemeireles.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.jorgemeireles.cursomc.domain.Cidade;
 import com.jorgemeireles.cursomc.domain.Cliente;
 import com.jorgemeireles.cursomc.domain.Endereco;
 import com.jorgemeireles.cursomc.domain.Estado;
+import com.jorgemeireles.cursomc.domain.Pagamento;
+import com.jorgemeireles.cursomc.domain.PagamentoComBoleto;
+import com.jorgemeireles.cursomc.domain.PagamentoComCartao;
+import com.jorgemeireles.cursomc.domain.Pedido;
 import com.jorgemeireles.cursomc.domain.Produto;
+import com.jorgemeireles.cursomc.domain.enums.EstadoPagamento;
 import com.jorgemeireles.cursomc.domain.enums.TipoCliente;
 import com.jorgemeireles.cursomc.repositories.CategoriaRepository;
 import com.jorgemeireles.cursomc.repositories.CidadeRepository;
 import com.jorgemeireles.cursomc.repositories.ClienteRepository;
 import com.jorgemeireles.cursomc.repositories.EnderecoRepository;
 import com.jorgemeireles.cursomc.repositories.EstadoRepository;
+import com.jorgemeireles.cursomc.repositories.PagamentoRepository;
+import com.jorgemeireles.cursomc.repositories.PedidoRepository;
 import com.jorgemeireles.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private EnderecoRepository enderecoRepository;
 	@Autowired
 	private ClienteRepository clienteRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -86,7 +98,26 @@ public class CursomcApplication implements CommandLineRunner {
 
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
-
+		
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+		Pedido ped1 = new Pedido (null, sdf.parse("30/09/2021 22:40"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("30/09/2021 22:56"), cli1, e2);
+		
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+        ped1.setPagamento(pagto1);
+        
+        Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2,sdf.parse("20/07/2017 00:00"), null);
+        ped2.setPagamento(pagto2);
+        
+        
+        cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+        
+        
+        pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1 , pagto2));
+        
 	}
 
 }
---
+++ src/main/java/com/jorgemeireles/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.jorgemeireles.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jorgemeireles.cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
